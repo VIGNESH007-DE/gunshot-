5cc276353dd446d63efbef05df5d4632fcc7cc7d
VIGNESH007-DE <vigneshwaranethicalhacker@gmail.com>
2025-10-07 11:21:44 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// PASTE YOUR FIREBASE CONFIGURATION OBJECT HERE
+// This object is provided by Firebase when you create a web app.
+// It should look like the example below.
+
+export const firebaseConfig = {
+  apiKey: "AIzaSy...YOUR_API_KEY",
+  authDomain: "your-project-id.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdef123456"
+};
